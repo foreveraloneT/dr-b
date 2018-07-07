eb8f950f2bf920845ebc5d1c985d3936fb18de9c
Sutee S <unlimited.tee@gmail.com>
2018-07-07 14:53:37 +0700

    json pertty

--- app/js/components/page/PatientReport/index.js
+++ app/js/components/page/PatientReport/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
+import moment from 'moment'
+import { omit } from 'lodash'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { compose } from 'redux'
 import { withRouter } from 'react-router-dom'
+import { Typography } from '@material-ui/core'
 
 import { TitleWithBackLayout } from '../../layout'
 import { Paper } from '../../common'
@@ -19,8 +22,37 @@ class PatientReport extends Component {
     careInfoList: PropTypes.array.isRequired,
   }
 
+  syntaxHighlight(text) {
+    let json
+    if (typeof text !== 'string') {
+      json = JSON.stringify(text, undefined, 2);
+    }
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+-]?\d+)?)/g, (match) => {
+      let cls = 'number'
+      if (/^"/.test(match)) {
+        if (/:$/.test(match)) {
+          cls = 'key'
+        } else {
+          cls = 'string'
+        }
+      } else if (/true|false/.test(match)) {
+        cls = 'boolean'
+      } else if (/null/.test(match)) {
+        cls = 'null'
+      }
+      return `<span class="${cls}">${match}</span>`
+    });
+  }
+
   render() {
     const { patient, history, careInfoList } = this.props
+    const careListToShow = careInfoList.map(data => ({
+      ...omit(data, ['patientId', 'isSync', 'createAt', 'updateAt']),
+      dateFull: moment(Number(data.date)).format('YYYY-MM-DD'),
+      dateShort: moment(Number(data.date)).format('dddd Do MMMM YYYY'),
+    }))
+
     return (
       <TitleWithBackLayout
         className="patient-container"
@@ -34,7 +66,12 @@ class PatientReport extends Component {
         }
       >
         <Paper>
-          {JSON.stringify(careInfoList, null, '\t')}
+          <Typography variant="headline" gutterBottom>
+            JSON Format
+          </Typography>
+          <pre className='code-container'>
+            <code dangerouslySetInnerHTML={{ __html: this.syntaxHighlight(careListToShow) }} />
+          </pre>
         </Paper>
       </TitleWithBackLayout>
     )
